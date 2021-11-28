990bbb7da013f35999ba88b705f63c174f9d18a8
hackerrank <me@hackerrank.com>
2021-11-27 21:34:39 -0500

    start of a database

---
+++ database.js
@@ -0,0 +1,29 @@
+// This ensures that things do not fail silently but will throw errors instead.
+"use strict";
+// Require better-sqlite.
+const Database = require('better-sqlite3');
+
+// Connect to a database or create one if it doesn't exist yet.
+const db = new Database('user.db');
+
+// Is the database initialized or do we need to initialize it?
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='AccountInfo';`);
+let row = stmt.get();
+if (row === undefined) {
+// Echo information about what you are doing to the console.
+    console.log('Your database appears to be empty. I will initialize it now.');
+// Set a const that will contain your SQL commands to initialize the database.
+    const sqlInit = `
+        CREATE TABLE accountinfo ( id INTEGER PRIMARY KEY autoincremenet, username TEXT, password TEXT,highscore INTEGER, numofgames INTEGER );
+		INSERT INTO acountinfo (username, password,highscore,numofgames) VALUES ('admin','bdc87b9c894da5168059e00ebffb9077',0,0)
+    `;
+// Execute SQL commands that we just wrote above.
+    db.exec(sqlInit);
+// Echo information about what we just did to the console.
+    console.log('Your database has been initialized with a new table and two entries containing a ID,username, password,highscore and numofgames');
+} else {
+// Since the database already exists, echo that to the console.
+    console.log('Database exists.')
+}
+// Export all of the above as a module so that we can use it elsewhere.
+module.exports = db
